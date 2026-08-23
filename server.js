ecd32db5ec891e4642b59bda4af50b060e4fece1
sanskar-cell <krishlifestylenandinipandekar@gmail.com>
2026-08-24 02:41:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 
 // =========================
